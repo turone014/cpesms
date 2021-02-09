bfa7464f04434f66e5c3730d7f043609a0b96ab7
turone014 <68006454+turone014@users.noreply.github.com>
2021-02-10 03:08:01 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php 
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'secretenemy1';
+$DATABASE_NAME = 'cpe';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+?>
