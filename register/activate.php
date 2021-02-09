2005b9d68c2ba84cbd6811745a49f561666f9dde
turone014 <68006454+turone014@users.noreply.github.com>
2021-02-10 03:10:11 +0800

    Add files via upload

---
+++ register/activate.php
@@ -0,0 +1,80 @@
+<?php
+// Change this to your connection info.
+require_once('../connection.php');
+if (mysqli_connect_errno()) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// First we check if the email and code exists...
+if (isset($_GET['email'], $_GET['code'])) {
+	if ($stmt = $con->prepare('SELECT * FROM cpe_students WHERE email = ? AND activation_code = ?')) {
+		$stmt->bind_param('ss', $_GET['email'], $_GET['code']);
+		$stmt->execute();
+		// Store the result so we can check if the account exists in the database.
+		$stmt->store_result();
+		if ($stmt->num_rows > 0) {
+			// Account exists with the requested email and code.
+			if ($stmt = $con->prepare('UPDATE cpe_students SET activation_code = ? WHERE email = ? AND activation_code = ?')) {
+				// Set the new activation code to 'activated', this is how we can check if the user has activated their account.
+				$newcode = 'activated';
+				$stmt->bind_param('sss', $newcode, $_GET['email'], $_GET['code']);
+				$stmt->execute();
+				$output ='Your account is now activated, you can now login!';
+			}
+		} else {
+			$output =  'The account is already activated or doesn\'t exist!';
+		}
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Computer Engineering Department</title>
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 login-section-wrapper">
+     
+         
+          
+          <div class="login-wrapper my-auto">
+            <img src="../assets/images/puplogo.png" alt="..." "="" style="
+    height: 107px;
+    padding-left: 31px;
+    padding-bottom: 6px;
+">
+<img src="../assets/images/cealogo.png" alt="..." "="" style="
+    height: 107px;
+    padding-left: 16px;
+    padding-bottom: 6px;
+"> <br><strong>
+     <?php echo $output ?>
+	 </strong>
+     <a href="../login" class="btn btn-block login-btn" role="button" >Sign in</a>
+           
+          </div>
+        </div>
+        <div class="col-sm-6 px-0 d-none d-sm-block">
+          <img src="../assets/images/login.jpg" alt="login image" class="login-img">
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
